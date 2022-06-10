378cc422fe67cec20f515c3d64a31841e5e2f319
Eldar Mirzabekov <prog.eldar@gmail.com>
2022-06-10 15:14:43 +0300

    feat(ci): setup gh action

--- apps/blog-api/src/app/app.controller.spec.ts
+++ apps/blog-api/src/app/app.controller.spec.ts
@@ -4,19 +4,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let app: TestingModule;
+  let appController: AppController;
+  let appService: AppService;
 
   beforeAll(async () => {
-    app = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
+
+    appService = moduleRef.get<AppService>(AppService);
+    appController = moduleRef.get<AppController>(AppController);
   });
 
   describe('getData', () => {
     it('should return "Welcome to blog-api!"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({
+      const data = appService.getData();
+
+      expect(data).toEqual({
         message: 'Welcome to blog-api!',
       });
     });
